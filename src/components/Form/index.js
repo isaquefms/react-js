85f3258f4315576e6034580ad2c4ffbbf2cca6fd
isaque <isaquefernando10@gmail.com>
2023-06-27 17:05:41 -0300

    iniciando repositório

---
+++ src/components/Form/index.js
@@ -0,0 +1,17 @@
+import './Form.css'
+import TextField from '../TextField'
+
+const Form = () => {
+    return (
+        <section className='form'>
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <TextField label="Nome" placeholder="Digite seu nome" />
+                <TextField label="Cargo" placeholder="Digite seu cargo" />
+                <TextField label="Imagem" placeholder="Digite sua imagem" />
+            </form>
+        </section>
+    )
+}
+
+export default Form;
---
+++ src/components/TextField/index.js
@@ -0,0 +1,14 @@
+import './TextField.css';
+
+const TextField = (props) => {
+    return (
+        <div className="textfield">
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default TextField;
