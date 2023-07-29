db7b0689b3145a7a60bc2f320676f6b3eba7c789
Isaque Fernando <isaquefernando10@gmail.com>
2023-07-29 00:08:24 -0300

    atualizações

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,48 @@
+import { useState } from 'react';
+
 import './Form.css'
 import TextField from '../TextField'
+import Dropdown from '../Dropdown';
+import Button from '../Button';
 
 const Form = () => {
+    const items = ['Front-end', 'Back-end', 'Full-stack', 'DevOps', 'Designer', 'Data Scientist', 'Mobile']
+
+    const [name, setName] = useState('')
+    const [role, setRole] = useState('')
+    const [image, setImage] = useState('')
+
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log('Salvou!', name, role, image)
+    }
+
     return (
         <section className='form'>
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextField label="Nome" placeholder="Digite seu nome" />
-                <TextField label="Cargo" placeholder="Digite seu cargo" />
-                <TextField label="Imagem" placeholder="Digite sua imagem" />
+                <TextField 
+                    required={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
+                    value={name}
+                    setValue={setName}
+                />
+                <TextField 
+                    required={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    value={role}
+                    setValue={setRole}    
+                />
+                <TextField
+                    label="Imagem"
+                    placeholder="Digite sua imagem"
+                    value={image}
+                    setValue={setImage}
+                />
+                <Dropdown required={true} label="Time" items={items}/>
+                <Button> Enviar </Button>
             </form>
         </section>
     )
--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './TextField.css';
 
 const TextField = (props) => {
+
+    const onInput = (event) => {
+        props.setValue(event.target.value)
+    }
+
     return (
         <div className="textfield">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input value={props.value} onChange={onInput} required={props.required} placeholder={props.placeholder}/>
         </div>
     )
 }
